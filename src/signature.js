bfb6c979fc0c6088cc3745e712f783585255abb0
Daniel Bacher <daniel.bacher@ergon.ch>
2016-11-24 19:31:48 +0100

    Add attribute for redraw and work empty image.

--- src/signature.js
+++ src/signature.js
@@ -6,67 +6,69 @@
 angular.module('signature', []);
 
 angular.module('signature').directive('signaturePad', ['$window',
-  function ($window) {
-    'use strict';
+    function ($window) {
+        'use strict';
 
-    var signaturePad, canvas, element, EMPTY_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA=';
-    return {
-      restrict: 'EA',
-      replace: true,
-      template: '<div class="signature" ng-style="{height: height + \'px\', width: width + \'px\'}"><canvas height="{{ height }}" width="{{ width }}"></canvas></div>',
-      scope: {
-        accept: '=',
-        clear: '=',
-        dataurl: '=',
-        height: '@',
-        width: '@'
-      },
-      controller: [
-        '$scope',
-        function ($scope) {
-          $scope.accept = function () {
-            var signature = {};
+        var signaturePad, canvas, element, EMPTY_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
+        return {
+            restrict: 'EA',
+            replace: true,
+            template: '<div class="signature" ng-click="ctrl.onSignatureDraw()" ng-style="{height: height + \'px\', width: width + \'px\'}"><canvas height="{{ height }}" width="{{ width }}"></canvas></div>',
+            scope: {
+                accept: '=',
+                clear: '=',
+                ondraw: '&ondraw',
+                dataurl: '=',
+                height: '@',
+                width: '@'
+            },
+            controller: [
+                '$scope',
+                function ($scope) {
+                    $scope.accept = function () {
+                        var signature = {};
 
-            if (!$scope.signaturePad.isEmpty()) {
-              signature.dataUrl = $scope.signaturePad.toDataURL();
-              signature.isEmpty = false;
-            } else {
-              signature.dataUrl = EMPTY_IMAGE;
-              signature.isEmpty = true;
+                        if (!$scope.signaturePad.isEmpty()) {
+                            signature.dataUrl = $scope.signaturePad.toDataURL();
+                            signature.isEmpty = false;
+                        } else {
+                            signature.dataUrl = EMPTY_IMAGE;
+                            signature.isEmpty = true;
+                        }
+
+                        return signature;
+                    };
+
+                    $scope.clear = function () {
+                        $scope.signaturePad.clear();
+                    };
+
+                    $scope.$watch("dataurl", function (dataUrl) {
+                        if (dataUrl) {
+                            console.log('SIGNATURE: DATA URL: ' + dataUrl);
+                            $scope.signaturePad.fromDataURL(dataUrl);
+                        }
+                    });
+                }
+            ],
+            link: function (scope, element) {
+                canvas = element.find('canvas')[0];
+                scope.signaturePad = new SignaturePad(canvas, {onBegin: scope.ondraw});
+
+                if (!scope.height) scope.height = 220;
+                if (!scope.width) scope.width = 568;
+
+                if (scope.signature && !scope.signature.$isEmpty && scope.signature.dataUrl) {
+                    scope.signaturePad.fromDataURL(scope.signature.dataUrl);
+                }
+
+                var ratio = Math.max($window.devicePixelRatio || 1, 1);
+                canvas.width = canvas.offsetWidth * ratio;
+                canvas.height = canvas.offsetHeight * ratio;
+                canvas.getContext("2d").scale(ratio, ratio);
             }
-
-            return signature;
-          };
-
-          $scope.clear = function () {
-            $scope.signaturePad.clear();
-          };
-
-          $scope.$watch("dataurl", function (dataUrl) {
-            if (dataUrl) {
-              $scope.signaturePad.fromDataURL(dataUrl);
-            }
-          });
-        }
-      ],
-        link: function (scope, element) {
-            canvas = element.find('canvas')[0];
-            scope.signaturePad = new SignaturePad(canvas);
-
-            if (!scope.height) scope.height = 220;
-            if (!scope.width) scope.width = 568;
-
-            if (scope.signature && !scope.signature.$isEmpty && scope.signature.dataUrl) {
-                scope.signaturePad.fromDataURL(scope.signature.dataUrl);
-            }
-
-            var ratio =  Math.max($window.devicePixelRatio || 1, 1);
-            canvas.width = canvas.offsetWidth * ratio;
-            canvas.height = canvas.offsetHeight * ratio;
-            canvas.getContext("2d").scale(ratio, ratio);
-        }
-    };
-  }
+        };
+    }
 ]);
 
 // Backward compatibility
